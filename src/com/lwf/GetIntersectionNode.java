8f6c89005922b3de2416785c0d9e893a46ca1397
Administrator <liuwulive@outlook.com>
2019-07-30 00:37:15 +0800

    23333 加油

--- src/com/lwf/GetIntersectionNode.java
+++ src/com/lwf/GetIntersectionNode.java
@@ -12,8 +12,8 @@ public class GetIntersectionNode {
 
 
  class ListNode {
-    int val;
-    ListNode next;
+  public  int val;
+   public ListNode next;
     ListNode(int x) {
         val = x;
         next = null;
