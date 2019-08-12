d6096fbe77ed7f832480b954d78ae93dab15afb4
liuwenfei <liuwulive@outlook.com>
2019-08-12 18:53:01 +0800

    415

---
+++ src/com/lwf/AddStrings.java
@@ -0,0 +1,11 @@
+package com.lwf;
+
+/**
+ * {@link 415.md}
+ */
+public class AddStrings {
+    public String addStrings(String num1, String num2) {
+        char[] chars1=num1.toCharArray();
+        char[] chars2=num2.toCharArray();
+    }
+}
