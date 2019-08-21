a591b2d43b7c8a82c231cd9abc591183d8eb70a2
Administrator <liuwulive@outlook.com>
2019-08-21 23:45:07 +0800

    理解题意再解题

---
+++ src/com/lwf/FindLUSlength.java
@@ -0,0 +1,13 @@
+package com.lwf;
+
+/**
+ * author Administrator
+ * time 2019-08-21-22:53
+ * {@link 521.md}
+ */
+public class FindLUSlength {
+    public int findLUSlength(String a, String b) {
+        if (a.equals(b)) return -1;
+        return Math.max(a.length(),b.length());
+    }
+}
---
+++ src/com/lwf/GetMinimumDifference.java
@@ -0,0 +1,24 @@
+package com.lwf;
+
+/**
+ * author Administrator
+ * time 2019-08-21-23:03
+ * {@link 530.md}
+ */
+public class GetMinimumDifference {
+    int min=Integer.MAX_VALUE;
+    TreeNode pre;
+    public int getMinimumDifference(TreeNode root) {
+        mid(root);
+        return min;
+    }
+    void mid(TreeNode node){
+        if (node==null) return;
+        mid(node.left);
+        if (pre!=null){
+            min= Math.min(min,node.val-pre.val);
+        }
+        pre=node;
+        mid(node.right);
+    }
+}
