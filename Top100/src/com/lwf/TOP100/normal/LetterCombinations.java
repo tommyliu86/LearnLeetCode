d8de8a6ba3ac444151add88bf59f241ac4795174
Administrator <liuwulive@outlook.com>
2019-09-25 23:53:50 +0800

    动态规划和递归模拟栈

---
+++ Top100/src/com/lwf/TOP100/normal/LetterCombinations.java
@@ -0,0 +1,38 @@
+package com.lwf.TOP100.normal;
+
+import javax.print.DocFlavor;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * author Administrator
+ * time 2019-09-25-22:44
+ * {@link 17.md}
+ */
+public class LetterCombinations {
+    private String[] strings = new String[]{"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+    public List<String> letterCombinations(String digits) {
+        List<String> lists = new ArrayList<>();
+        if (digits==null||digits.length()==0) return lists;
+        char[] chars = digits.toCharArray();
+
+        char[] gets = new char[chars.length];
+        backTracking(chars, gets, 0, lists);
+        return lists;
+    }
+
+    void backTracking(char[] res, char[] gets, int i, List<String> lists) {
+        if (i >= res.length) {
+
+            lists.add(String.valueOf(gets, 0, i));
+            return;
+        }
+        String resStr = strings[res[i] - '2'];
+        for (char c : resStr.toCharArray()) {
+            gets[i] = c;
+            backTracking(res, gets, i + 1, lists);
+
+        }
+    }
+}
