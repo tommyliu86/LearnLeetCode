3a44b71aad79685b0cb1044a1246ca58d4e72285
Administrator <liuwulive@outlook.com>
2019-10-23 23:27:37 +0800

    太困了，明天体检，早起，留到明天做

---
+++ Top100/src/com/lwf/TOP100/normal/Exist.java
@@ -0,0 +1,23 @@
+package com.lwf.TOP100.normal;
+
+/**
+ * author Administrator
+ * time 2019-10-23-23:22
+ * {@link md/normal/79.md}
+ */
+public class Exist {
+    public boolean exist(char[][] board, String word) {
+        int x=board.length;
+        int y=board[0].length;
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                if (board[i][j]==word.charAt(0)){
+
+                }
+            }
+        }
+    }
+  boolean  backTracking(char[][] board,String word,int x,int y){
+
+    }
+}
