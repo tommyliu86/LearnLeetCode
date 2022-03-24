35b038e5e8bde99cf1c783db43be80b52c17f295
liuwenfei14 <liuwenfei14@jd.com>
2022-03-24 09:30:21 +0800

    feat:图的基础，并查集

--- Top100/src/com/lwf/TOP100/normal/FindCircleNum.java
+++ Top100/src/com/lwf/TOP100/normal/FindCircleNum.java
@@ -12,11 +12,11 @@ import java.util.Queue;
  * @date: 2022-03-21 10:06
  */
 public class FindCircleNum {
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[][] ins = new int[][]{new int[]{1, 0, 0, 1}, new int[]{0, 1, 1, 0}, new int[]{0, 1, 1, 1}, new int[]{1, 0, 1, 1}};
-        int circleNum = solution.findCircleNum(ins);
-        System.out.println(circleNum);
+    /**
+    *
+     */
+    static class unionFind {
+
     }
 
     /**
@@ -111,7 +111,7 @@ public class FindCircleNum {
      * 3.每个顶点，对和其相连顶点进行深度向下遍历，一次找到所有相连顶点。构成一个图
      * 4.在遍历过程中注意统计遍历到的节点个数做边界终止条件即可。
      */
-    static class Solution {
+    static class dfs {
         Integer markCount = 0;
 
         public int findCircleNum(int[][] isConnected) {
