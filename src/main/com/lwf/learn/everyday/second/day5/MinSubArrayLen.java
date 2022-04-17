de7d326b22f8f87e0ca48d0c08b963b23f03e163
liuwenfei14 <liuwenfei14@jd.com>
2022-04-17 12:30:27 +0800

    feat:滑动窗口是双指针法的一种，可以在这种思路模式下大大简化代码逻辑，主要是思路！

---
+++ src/main/com/lwf/learn/everyday/second/day5/MinSubArrayLen.java
@@ -0,0 +1,34 @@
+package com.lwf.learn.everyday.second.day5;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: liuwenfei14
+ * @date: 2022-04-17 12:12
+ */
+public class MinSubArrayLen {
+    /**
+     * 双指针--滑动窗口法
+     */
+    class Solution {
+        public int minSubArrayLen(int target, int[] nums) {
+            int sum=0;
+            int left=0;
+            int right=0;
+            int min=nums.length+1;
+            while (right<nums.length){
+                sum+=nums[right];
+                if (sum>=target){
+
+                    while (left<nums.length&& sum>=target){
+                        sum-=nums[left];
+                        left++;
+                    }
+                    min=Math.min(min,right-left+2);
+                }
+                right++;
+            }
+            return min==nums.length+1?0:min;
+        }
+    }
+}
