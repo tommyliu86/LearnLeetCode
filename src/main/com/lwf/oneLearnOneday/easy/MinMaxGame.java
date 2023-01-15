c1a0f98f6ab8017ab22b36c696b8f326d0c23da8
liuwenfei <liuwulive@outlook.com>
2023-01-15 11:16:04 +0800

    feat:2023年1月15日

---
+++ src/main/com/lwf/oneLearnOneday/easy/MinMaxGame.java
@@ -0,0 +1,22 @@
+package com.lwf.oneLearnOneday.easy;
+
+import com.lwf.oldtime.NumArray;
+import sun.text.resources.cldr.hi.FormatData_hi;
+
+public class MinMaxGame {
+    /**
+     * 模拟计算
+     */
+    class Solution {
+        public int minMaxGame(int[] nums) {
+            int i=nums.length;
+            while (i!=1){
+                for (int j = 0; j < i/2; j++) {
+                    nums[j]= ((j&1)==0)?Math.min(nums[j*2],nums[j*2+1]):Math.max(nums[j*2],nums[j*2+1]);
+                }
+                i=i/2;
+            }
+            return nums[0];
+        }
+    }
+}
