2af6bd4f196cb23da54cadbe3755776f785d5f46
liuwenfei <liuwulive@outlook.com>
2023-02-08 19:40:09 +0800

    数学

---
+++ src/main/com/lwf/offer/one/ValidateStackSequences.java
@@ -0,0 +1,33 @@
+package com.lwf.offer.one;
+
+import java.util.Stack;
+
+/**
+ * @author liuwenfei
+ * @date 2023/2/8 15:37
+ */
+public class ValidateStackSequences {
+    /**
+     * 模拟
+     */
+    class Solution {
+        public boolean validateStackSequences(int[] pushed, int[] popped) {
+            Stack<Integer> stack=new Stack<>();
+            int i=0;
+            int j=0;
+            while (i<popped.length){
+                int pop = popped[i];
+                while (j<pushed.length&& stack.isEmpty()||stack.peek()!=pop){
+                    stack.push(pushed[j++]);
+                }
+                if (stack.peek()==pop){
+                    stack.pop();
+                    i++;
+                }else{
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
