5c5326a64457f8c68885a234725b06771b1417b7
liuwenfei <liuwulive@outlook.com>
2023-08-28 09:17:33 +0800

    2023年8月28日

--- src/main/com/lwf/oneLearnOneday/normal/Insert.java
+++ src/main/com/lwf/oneLearnOneday/normal/Insert.java
@@ -1,5 +1,8 @@
 package com.lwf.oneLearnOneday.normal;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -7,80 +10,108 @@ package com.lwf.oneLearnOneday.normal;
  * @date: 2022-06-18 14:27
  */
 public class Insert {
-    /*
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node next;
-
-    public Node() {}
-
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, Node _next) {
-        val = _val;
-        next = _next;
-    }
-};
-*/
 
     class Solution {
+        //start《ns《end 合并 到 start《nn《end or nn《start
+        public int[][] insert(int[][] intervals, int[] newInterval) {
+            if (intervals.length==0){
+                return new int[][]{newInterval};
+            }
+            List<int[]> lists=new ArrayList<>();
+            boolean merged=false;
+            for (int i = 0; i < intervals.length; i++) {
+                int start=intervals[i][0];
+                int end=intervals[i][1];
+                if (merged||end<newInterval[0]){
+                    lists.add(intervals[i]);
+                    continue;
+                }
+
+                if (newInterval[1]<start){
+                    lists.add(newInterval);
+                    lists.add(intervals[i]);
+                }else{
+                    start=Math.min(start,newInterval[0] );
+                    end=Math.max(end,newInterval[1]);
+                    int j=i+1;
+                    while (j<intervals.length&&intervals[j][0]<=end){
+                        end=Math.max(end,intervals[j][1]);
+                        j++;
+                    }
+                    lists.add(new int[]{start,end});
+                    i=j-1;
+                }
+                merged=true;
+
+            }
+            if (!merged){
+                lists.add(newInterval);
+            }
+            int[][] ans=new int[lists.size()][2];
+            for (int i = 0; i < ans.length; i++) {
+                ans[i]=lists.get(i);
+            }
+            return ans;
+        }
+    }
+
+    class Solution1 {
         public Node insert(Node head, int insertVal) {
-            if (head==null){
+            if (head == null) {
                 Node node = new Node(insertVal);
-                node.next=node;
+                node.next = node;
                 return node;
             }
-            Node cur=head;
-            Node next=head.next;
-            if (next==cur){
+            Node cur = head;
+            Node next = head.next;
+            if (next == cur) {
                 Node node = new Node(insertVal);
-                cur.next=node;
-                node.next=next;
+                cur.next = node;
+                node.next = next;
                 return cur;
-            }else{
-                boolean find    =false;
-                while (next!=head){
+            } else {
+                boolean find = false;
+                while (next != head) {
 
-                    if (cur.val>next.val){
-                        if (insertVal>=cur.val||insertVal<=next.val) {
+                    if (cur.val > next.val) {
+                        if (insertVal >= cur.val || insertVal <= next.val) {
                             Node node = new Node(insertVal);
-                            cur.next=node;
-                            node.next=next;
-                            find=true;
+                            cur.next = node;
+                            node.next = next;
+                            find = true;
                             break;
                         }
-                    }else{
-                        if (insertVal>=cur.val&&insertVal<=next.val){
+                    } else {
+                        if (insertVal >= cur.val && insertVal <= next.val) {
                             Node node = new Node(insertVal);
-                            cur.next=node;
-                            node.next=next;
-                            find=true;
+                            cur.next = node;
+                            node.next = next;
+                            find = true;
                             break;
                         }
                     }
 
-                    cur=cur.next;
-                    next=next.next;
+                    cur = cur.next;
+                    next = next.next;
                 }
-                if (!find){
+                if (!find) {
 
                     Node node = new Node(insertVal);
-                    cur.next=node;
-                    node.next=next;
+                    cur.next = node;
+                    node.next = next;
                 }
                 return head;
             }
 
         }
     }
+
     class Node {
         public int val;
         public Node next;
 
-        public Node() {}
+        public Node() {
+        }
 
         public Node(int _val) {
             val = _val;
