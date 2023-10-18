641194ebde1892465e042beeca0411ca21246456
liuwenfei <liuwenfei@xiaomi.com>
2023-10-18 09:58:26 +0800

    2023年10月18日

--- src/main/com/lwf/oneLearnOneday/easy/SumOfMultiples.java
+++ src/main/com/lwf/oneLearnOneday/easy/SumOfMultiples.java
@@ -6,9 +6,22 @@ package com.lwf.oneLearnOneday.easy;
  */
 public class SumOfMultiples {
     /**
-     * 直接遍历
+     * 容斥原理，等差数列求和公式直接计算
      */
     class Solution {
+        public int sumOfMultiples(int n) {
+            int ans=0;
+            ans=sum(n,3)+sum(n,5)+sum(n,7)-sum(n,3*5)-sum(n,3*7)-sum(n,5*7)+sum(n,3*5*7);
+            return ans;
+        }
+        public int sum(int n,int c){
+            return c*((n/c)+1)*(n/c)/2;
+        }
+    }
+    /**
+     * 直接遍历
+     */
+    class Solution1 {
         public int sumOfMultiples(int n) {
             int ans=0;
             for (int i = 1; i <= n; i++) {
